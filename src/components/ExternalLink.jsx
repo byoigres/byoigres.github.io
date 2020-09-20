8558cbd0735f9b40be5a3570191d0d239207acda
Sergio Flores <byoigres@gmail.com>
2020-09-20 01:58:36 -0600

    Added children to component

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const ExternalLink = ({ uri, children }) => (
-  <a href="uri" target="_blank" rel="nofollow noopener noreferrer">
+  <a href={uri} target="_blank" rel="nofollow noopener noreferrer">
     {children}
   </a>
 )
