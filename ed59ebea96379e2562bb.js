552fc46e2d2d0430ae53d560c97150d2809a2584
Sergio Flores <byoigres@gmail.com>
2020-09-28 14:17:16 -0600

    Updates

--- ed59ebea96379e2562bb.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xc6f0978e888a],{440:function(e,a){e.exports={data:{site:{siteMetadata:{disqusShortname:"byoigres"}},markdownRemark:{id:"/Users/sergio/Projects/byoigres/byoigres.github.io/src/pages/pages/acerca-de-mi.md absPath of file >>> MarkdownRemark",html:'<p>Hola, mi nombre es Sergio Flores, soy programador y me encanta aprender cosas nuevas.</p>\n<p><a href="/contacto">Contactame</a></p>',frontmatter:{date:null,path:"/acerca-de-mi/",title:"Acerca de mi"}}},pathContext:{}}}});
-//# sourceMappingURL=ed59ebea96379e2562bb.js.map
